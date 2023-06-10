7d1c4777d7927e621eebeb2d6bfbe095fd520816
Mouhoub Merdas <mouhoub.merdas@se.univ-bejaia.dz>
2023-06-10 19:47:51 +0100

    Interface recup mot de passe corrigé

--- app/src/main/java/com/example/ilauriaartisan_v1/RecuperationMotDePasse.java
+++ app/src/main/java/com/example/ilauriaartisan_v1/RecuperationMotDePasse.java
@@ -9,21 +9,27 @@ import android.widget.EditText;
 
 
 public class RecuperationMotDePasse extends AppCompatActivity {
-    private Button b;
+    private Button btnRessai;
     private EditText t1,t2,t3,t4,t5,t6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b=findViewById(R.id.btnc);
-        t1=findViewById(R.id.e1);
-        t2=findViewById(R.id.e2);
-        t3=findViewById(R.id.e3);
-        t4=findViewById(R.id.e4);
-        t5=findViewById(R.id.e5);
-        t6=findViewById(R.id.e6);
-        b.setVisibility(View.GONE);
+        setContentView(R.layout.activity_recuperation_mot_de_passe);
+
+
+        btnRessai=findViewById(R.id.btnReessayer);
+        btnRessai.setVisibility(View.GONE);
+
+
+        t1=findViewById(R.id.champs1);
+        t2=findViewById(R.id.champs2);
+        t3=findViewById(R.id.champs3);
+        t4=findViewById(R.id.champs4);
+        t5=findViewById(R.id.champs5);
+        t6=findViewById(R.id.champs6);
+
+
     }
     public void v(View view)
     {
@@ -35,7 +41,7 @@ public class RecuperationMotDePasse extends AppCompatActivity {
         String s6=t6.getText().toString();
 
         if(s1.equals("")&& s2.equals("") && s3.equals("") && s4.equals("") && s5.equals("") && s6.equals("")) {
-            b.setVisibility(View.VISIBLE);
+            btnRessai.setVisibility(View.VISIBLE);
         }
         else{
             //verification base de données
