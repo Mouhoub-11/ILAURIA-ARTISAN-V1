8792868c21c6b5ba9dbac66002da0fd303c0c0a0
Mouhoub Merdas <mouhoub.merdas@se.univ-bejaia.dz>
2023-06-10 11:02:21 +0100

    boutton se decconnecter CLIENT

--- app/src/main/java/com/example/ilauriaartisan_v1/Favoris.java
+++ app/src/main/java/com/example/ilauriaartisan_v1/Favoris.java
@@ -2,10 +2,14 @@ package com.example.ilauriaartisan_v1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,6 +29,52 @@ public class Favoris extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoris);
+
+
+        Toolbar toolbar=findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(Favoris.this, toolbar);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_list_item, popupMenu.getMenu());
+
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int itemId = item.getItemId();
+
+                        switch (itemId) {
+
+                            case R.id.btnSeDeco:
+                                sedecconecter();
+                                return true;
+
+
+                            default:
+                                return false;
+                        }
+
+
+                    }
+                });
+
+
+
+
+                popupMenu.show();
+            }
+        });
+
+
+
+    }
+
+    public void sedecconecter(){
+        Intent i7=new Intent(Favoris.this, Authentification.class);
+        startActivity(i7);
+
 
 
     }
